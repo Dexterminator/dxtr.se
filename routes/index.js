59d68802bb398a50d9b97a41de03234e64d185a1
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-05-20 09:04:45 +0200

    Added routes.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/projects', function(req, res, next) {
+  res.render('projects');
+});
+
+router.get('/cv', function(req, res, next) {
+  res.render('cv');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
 module.exports = router;
