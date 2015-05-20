c7d0a7640728b7587c5645785de2da967b706e2c
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-05-20 11:35:17 +0200

    Navbar improvements, active page is highlighted.

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {currentPage: 'index'});
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects');
+  res.render('projects', {currentPage: 'projects'});
 });
 
 router.get('/cv', function(req, res, next) {
-  res.render('cv');
+  res.render('cv', {currentPage: 'cv'});
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact');
+  res.render('contact', {currentPage: 'contact'});
 });
 
 module.exports = router;
