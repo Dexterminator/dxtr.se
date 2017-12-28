e4e92698389a709e5859227a5852cfe6c8744ee4
Dexter Gramfors <dexter.gramfors@gmail.com>
2017-12-28 10:08:37 +0100

    Force ssl redirect, and update deps.

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var lessMiddleware = require("less-middleware");
+var sslRedirect = require('heroku-ssl-redirect');
+
 var app = express();
+
+app.use(sslRedirect());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
