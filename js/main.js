73f707702010a1405a1d98e02a5bca6adbb99141
Dexter Gramfors <dexter.gramfors@gmail.com>
2018-05-12 17:52:43 +0200

    Re-enable scroll to headers on nav clicks.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+function scrollToId(id) {
+  var element = document.getElementById(id);
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'nearest'
+  });
+}
+
+function scrollToTop() {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
 function startImageFades() {
   var meImages = document.getElementById('me-images').children;
   var i = 0;
@@ -8,6 +24,34 @@ function startImageFades() {
   }, 4000);
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  startImageFades()
+var navs = ['contact', 'projects', 'cv'];
+
+function getCurrentActiveNav() {
+  var scrolled = window.scrollY + 40;
+  var currentNavName = navs.find(function (navName) {
+    return scrolled > document.getElementById(navName + '-header').offsetTop;
+  });
+
+  if (currentNavName) {
+    return currentNavName + '-nav'
+  }
+
+  return 'me-nav'
+}
+
+function setNavActiveStatusOnScroll() {
+  var currentNav = 'me-nav';
+  window.addEventListener('scroll', function (e) {
+    var newNav = getCurrentActiveNav();
+    if (newNav !== currentNav) {
+      document.getElementById(currentNav).classList.toggle('active');
+      document.getElementById(newNav).classList.toggle('active');
+      currentNav = newNav;
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  startImageFades();
+  setNavActiveStatusOnScroll();
 });
